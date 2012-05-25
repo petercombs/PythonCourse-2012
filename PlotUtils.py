e5219bddc9115eb23a101e683983f3e250ca7772
Peter Combs <peter.combs@berkeley.edu>
2012-05-25 15:06:04 -0700

    Allow arbitrary scaling of coding region in plot_averaged_genes
    Do this by use of a bp_equiv argument, which says that the CDS area
    should be equivalent to that many basepairs of the upstream or
    downstream region.
    
    This also has the advantage of removing magic numbers from the body of
    the function, which should help make the code more comprehensible to
    others.

--- PlotUtils.py
+++ PlotUtils.py
@@ -1,19 +1,19 @@
 from matplotlib import pyplot as mpl
 import numpy as np
 
-def relabel(tick):
+def relabel(tick, bp_equiv):
     if tick < 0:
         return "%d bp" % tick
     elif tick == 0:
         return "-0 bp/0%"
-    elif tick < 100:
-        return "%d%%" % tick
-    elif tick == 100:
+    elif tick < bp_equiv:
+        return "%d%%" % (float(tick)/bp_equiv * 100)
+    elif tick == bp_equiv:
         return '100%/+0 bp'
     else:
-        return "+%d bp" % (tick - 100)
+        return "+%d bp" % (tick - bp_equiv)
 
-def plot_averaged_genes(upstream, gene, downstream):
+def plot_averaged_genes(upstream, gene, downstream, bp_equiv = 200):
     """ Plots the coverage around genes.
 
     Here, the upstream and downstream regions are assumed to be in base pairs,
@@ -28,18 +28,18 @@ def plot_averaged_genes(upstream, gene, downstream):
     retval.append(ax.plot(np.arange(-len(upstream), 0),
                            upstream,
                            label='Upstream'))
-    retval.append(ax.plot(np.linspace(0, 100, len(gene)),
+    retval.append(ax.plot(np.linspace(0, bp_equiv, len(gene)),
                            gene,
                            label='Gene'))
-    retval.append(ax.plot(np.arange(100, len(downstream) + 100),
+    retval.append(ax.plot(np.arange(bp_equiv, len(downstream) + bp_equiv),
                            downstream,
                            label='Downstream'))
     ymin, ymax = ax.get_ybound()
-    retval.append(ax.vlines([0, 100], ymin, ymax , linestyles='dashed'))
+    retval.append(ax.vlines([0, bp_equiv], ymin, ymax , linestyles='dashed'))
     ticks = list(ax.get_xticks())
-    ticks.extend([0,50,100]) # Ensure we have at least the full gene labelled
+    ticks.extend([0,bp_equiv/2,bp_equiv]) # Ensure we have at least the full gene labelled
     ticks = np.unique(ticks)
-    ticklabels = [relabel(tick) for tick in ticks]
+    ticklabels = [relabel(tick, bp_equiv) for tick in ticks]
     ax.set_xticks(ticks)
     ax.set_xticklabels(ticklabels)
     mpl.show()
