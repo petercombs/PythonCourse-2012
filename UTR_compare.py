9e4df5fce3ae26e51d09a0ea96eba36e40fd5eaa
Peter Combs <peter.combs@berkeley.edu>
2012-06-15 16:42:48 -0700

    Add a clearance variable to the command line options as well

--- UTR_compare.py
+++ UTR_compare.py
@@ -8,8 +8,6 @@ import argparse
 from Bio import SeqIO
 
 GENOME_SIZE = 4639675  # E. coli K12 MG1655 from Ensembl
-DOWNSTREAM = 50
-DOWNSTREAM_KEEP = 100
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -22,6 +20,7 @@ def parse_args():
     parser.add_argument('--downstream_start', '-S', type=int, default=0)
     parser.add_argument('--downstream_end', '-E', type=int, default=100)
     parser.add_argument('--downstream_region', '-D', type=int, default=50)
+    parser.add_argument('--downstream_cleareance', '-C', type=int, default=50)
     parser.add_argument('--ratio-high', '-R', type=float, default=1e100)
     parser.add_argument('--ratio_low', '-r', type=float, default=10.0)
     parser.add_argument('--keep-neg', dest='keep_pos', default=1,
@@ -70,7 +69,8 @@ if __name__ == "__main__":
         gene_data = {dat.split()[0] : dat.split()[1] for dat in gene_data if dat}
         gene_id = gene_data.get('gene_name', 'unknown_gene').strip('"')
         if gene.strand == '+':
-            if gene.end + args.downstream_region > nearest_to_right[gene.end + 3]:
+            if ((gene.end + args.downstream_region)
+                > (nearest_to_right[gene.end + 3] - args.downstream_clearance)):
                 continue
             drug_pileup = drug_reads.get_as_array(gene.chrom, gene.end,
                                             gene.end+args.downstream_region)
@@ -90,7 +90,8 @@ if __name__ == "__main__":
                         SeqIO.write(seq, args.outfh, 'fasta')
                         outputted_data.append(gene_id)
         elif gene.strand == '-':
-            if gene.start - args.downstream_region < nearest_to_left[gene.start - 3]:
+            if ((gene.start - args.downstream_region)
+                < (nearest_to_left[gene.start - 3] + args.downstream_clearance)):
                 continue
             drug_pileup = drug_reads.get_as_array(gene.chrom,
                                             gene.start - args.downstream_region,
