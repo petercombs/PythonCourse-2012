5cc3ad6c8dfca18f39028863f823788596869ccb
Peter Combs <peter.combs@berkeley.edu>
2012-06-15 12:17:31 -0700

    Look for strings of arbitrary length in FFTSeq
    Take the elements as separate items on the command line, then using a
    couple functions, find all occurrences of the indicated substrings.

--- FFTSeq.py
+++ FFTSeq.py
@@ -1,11 +1,26 @@
 from Bio import SeqIO
 import sys
 
-is_one = sys.argv[2]
+is_one = sys.argv[2:]
+
+def find_all(string, substring):
+    loc = string.find(substring)
+    locs = []
+    while loc != -1:
+        locs.append(loc)
+        loc = string.find(substring, loc + 1)
+    return locs
+
+def find_all_from_list(string, list):
+    all_locs = []
+    for substring in list:
+        all_locs.extend(find_all(string, substring))
+    return all_locs
 
 
 all_seqs = []
 
 for rec in SeqIO.parse(sys.argv[1], 'fasta'):
-    seq = [(1 if base in is_one else 0) for base in rec.seq]
+    hits = find_all_from_list(rec.seq, is_one)
+    seq = [(1 if i in hits else 0) for i in range(len(rec))]
     all_seqs.append(seq)
